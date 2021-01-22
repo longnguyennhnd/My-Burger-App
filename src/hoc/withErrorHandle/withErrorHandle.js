cfdc939657c2929bf9a48e362926d0029758cc5e
Long Nguyen <70678347+longnguyennhnd@users.noreply.github.com>
2021-01-22 16:50:23 +0700

    183. Handing Errors

--- src/hoc/withErrorHandle/withErrorHandle.js
+++ src/hoc/withErrorHandle/withErrorHandle.js
@@ -1,2 +1,40 @@
+import React,{Component} from 'react';
+import Modal from '../../components/UI/Modal/Modal';
+import Aux from '../Auxiliary/Auxiliary';
+
+const withErrorHandle = (WrappedComponent, axios) => {
+    return class extends Component{
+        state={
+            error: null
+        }
+        componentDidMount(){
+            axios.interceptors.request.use(req => {
+                this.setState({error: null});
+                return req;
+            })
+            axios.interceptors.response.use(res => res, error => {
+                this.setState({error: error})
+            })
+        }
+
+        errorConfirmedHandle = () => {
+            this.setState({error: null});
+        }
+
+        render(){
+            return(
+                <Aux>
+                    <Modal 
+                    show={this.state.error}
+                    modalClosed={this.errorConfirmedHandle}>
+                        {this.state.error ? this.state.error.messege:  null}
+                    </Modal>
+                    <WrappedComponent {...this.props} />
+                </Aux>
+            );
+        }
+    }
+}
 
 
+export default withErrorHandle;
