4ab19c6c83b712b24a4aff06cd29b283e1283a62
Long Nguyen <70678347+longnguyennhnd@users.noreply.github.com>
2021-03-05 14:45:54 +0700

    327. Adding Sign-In

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -21,15 +21,19 @@ export const authFail = (error) => {
     };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
             email: email,
             password: password,
             returnSecureToken: true
+        };
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBs1lsEKilKrX9wAH5dSFimF6cw9mQ2m0k';
+        if(!isSignup){
+            url='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBs1lsEKilKrX9wAH5dSFimF6cw9mQ2m0k'
         }
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBs1lsEKilKrX9wAH5dSFimF6cw9mQ2m0k', authData)
+        axios.post(url, authData)
         .then(response => {
             console.log(response);
             dispatch(authSuccess(response.data));
