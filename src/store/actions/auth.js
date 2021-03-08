4d1ab331adf7df3da4460374e0a7c0be6a432040
Long Nguyen <70678347+longnguyennhnd@users.noreply.github.com>
2021-03-08 15:39:54 +0700

    337. Fixing Connect + Routing Errors

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -48,28 +48,28 @@ export const auth = (email, password, isSignup) => {
             returnSecureToken: true
         };
         let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBs1lsEKilKrX9wAH5dSFimF6cw9mQ2m0k';
-        if(!isSignup){
-            url='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBs1lsEKilKrX9wAH5dSFimF6cw9mQ2m0k'
+        if (!isSignup) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBs1lsEKilKrX9wAH5dSFimF6cw9mQ2m0k'
         }
         axios.post(url, authData)
-        .then(response => {
-            console.log(response);
-            const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
-            localStorage.setItem('token', response.data.idToken);
-            localStorage.setItem('expirationDate', expirationDate);
-            localStorage.setItem('userId', response.data.localId)
-            dispatch(authSuccess(response.data.idToken, response.data.localId));
-            dispatch(checkAuthTimeout(response.data.expiresIn));
-        })
-        .catch(err => {
-            console.log(err);
-            dispatch(authFail(err.response.data.error));
-        })
+            .then(response => {
+                console.log(response);
+                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('expirationDate', expirationDate);
+                localStorage.setItem('userId', response.data.localId)
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err.response.data.error));
+            })
     };
 };
 
 
-export const setAuthRedirectPath = (path) =>{
+export const setAuthRedirectPath = (path) => {
     return {
         type: actionTypes.SET_AUTH_REDIRECT_PATH,
         path: path
@@ -79,18 +79,18 @@ export const setAuthRedirectPath = (path) =>{
 export const authCheckState = () => {
     return dispatch => {
         const token = localStorage.getItem('token');
-        if(!token){
+        if (!token) {
             dispatch(logout());
-        }else{
+        } else {
             const expirationDate = new Date(localStorage.getItem('expirationDate'));
-            if(expirationDate <= new Date()){
+            if (expirationDate <= new Date()) {
                 dispatch(logout());
-            }else{
+            } else {
                 const userId = localStorage.getItem('userId');
                 dispatch(authSuccess(token, userId));
-                dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()))
+                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000))
             }
-            
+
         }
     };
 };
